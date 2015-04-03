83abe76937e8751259d70da3df74e0f50a0c6b60
qhenkart <qhenkart@gmail.com>
2015-04-03 16:51:07 -0700

    Created enemies and gameboard

--- watchout.js
+++ watchout.js
@@ -2,7 +2,7 @@
 var gameOptions = {
   height: 450,
   width: 700,
-  nEnemies: 30,
+  nEnemies: 40,
   padding: 20
 }
 
@@ -11,52 +11,29 @@ var gameStats = {
   bestScore: 0
 }
 
-
-
-
-/*
-var axes = {
-  x: d3.scale.linear().domain([0,100]).range([0, gameOptions.width]),
-  y: d3.scale.linear().domain([0,100]).range([0, gameOptions.height])
-}
-
-var gameBoard = d3.select('.container').append('svg:svg')
-      .attr('width', gameOptions.width)
-      .attr('height, gameOptions.height');
-
-
-var svg = d3.select("body").append("svg");
-
-var updateScore = d3.select(".current").text(gameStats.score.toString());
-
 var updateBestScore = function(){
   gameStats.bestScore = _.max(gameStats.bestScore, gameStats.score)
   d3.select('#best-score').text(gameStats.bestScore.toString());
 }
 
+// initialize board
+var svg = d3.select("body").append("svg")
+    .attr("width", gameOptions.width)
+    .attr("height", gameOptions.height)
+    .append("rect")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("fill", "white");
 
-var Player = function(gameOptions){
-  // must insert a path here
-
-
-  this.fill = '#ff6600';
-  this.x = 0;
-  this.y = 0;
-  this.angle = 0;
-  this.r = 5
-  this.gameOptions = gameOptions;
-
-}
-
-Player.prototype.render = function(to){
-  var el = to.append('svg:path').attr('d', @path).attr('fill', @fill)
-}
-
-*/
-
-
-
-
+//create enemies
+var circles = d3.select("svg").selectAll('circle')
+.append("circle").data(_.range(0, gameOptions.nEnemies), function(d){
+  return d;
+}).enter().append('circle')
+    .attr('cx', function(x){return Math.random() * gameOptions.width + 1}) //positions
+    .attr('cy', function(x){return Math.random() * gameOptions.height + 1})
+    .attr('r', 10) //size
+    .attr('fill', 'red');
 
 
 
