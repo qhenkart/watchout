5fa79e34a724b2c64e1d0b9e1d49a45db9578046
qhenkart <qhenkart@gmail.com>
2015-04-03 15:26:10 -0700

    nothing to add

--- watchout.js
+++ watchout.js
@@ -11,6 +11,10 @@ var gameStats = {
   bestScore: 0
 }
 
+
+
+
+/*
 var axes = {
   x: d3.scale.linear().domain([0,100]).range([0, gameOptions.width]),
   y: d3.scale.linear().domain([0,100]).range([0, gameOptions.height])
@@ -27,9 +31,28 @@ var updateScore = d3.select(".current").text(gameStats.score.toString());
 
 var updateBestScore = function(){
   gameStats.bestScore = _.max(gameStats.bestScore, gameStats.score)
+  d3.select('#best-score').text(gameStats.bestScore.toString());
 }
 
 
+var Player = function(gameOptions){
+  // must insert a path here
+
+
+  this.fill = '#ff6600';
+  this.x = 0;
+  this.y = 0;
+  this.angle = 0;
+  this.r = 5
+  this.gameOptions = gameOptions;
+
+}
+
+Player.prototype.render = function(to){
+  var el = to.append('svg:path').attr('d', @path).attr('fill', @fill)
+}
+
+*/
 
 
 
