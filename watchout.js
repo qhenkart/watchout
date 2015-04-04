c27ca19d4d0f696b818b0641792c66a6985b99ce
qhenkart <qhenkart@gmail.com>
2015-04-03 18:02:27 -0700

    added enemy movement

--- watchout.js
+++ watchout.js
@@ -11,6 +11,7 @@ var gameStats = {
   bestScore: 0
 }
 
+
 var updateBestScore = function(){
   gameStats.bestScore = _.max(gameStats.bestScore, gameStats.score)
   d3.select('#best-score').text(gameStats.bestScore.toString());
@@ -26,14 +27,38 @@ var svg = d3.select("body").append("svg")
     .attr("fill", "white");
 
 //create enemies
-var circles = d3.select("svg").selectAll('circle')
-.append("circle").data(_.range(0, gameOptions.nEnemies), function(d){
-  return d;
-}).enter().append('circle')
-    .attr('cx', function(x){return Math.random() * gameOptions.width + 1}) //positions
-    .attr('cy', function(x){return Math.random() * gameOptions.height + 1})
-    .attr('r', 10) //size
+var Enemies = function(){
+
+}
+
+Enemies.prototype.createProperties = function(){
+  var enemyData = [];
+  for (var i = 0; i < gameOptions.nEnemies; i++){
+    enemyData.push({r: 10, x: Math.random() * gameOptions.width + 1, y: Math.random() * gameOptions.height + 1});
+  }
+  return enemyData;
+}
+
+
+Enemies.prototype.createEnemies = function(){
+  d3.select("svg").selectAll('circle')
+    .append("circle")
+    .data(context)
+    .enter().append('circle')
+    .attr('cx', function(d){return d.x}) //positions
+    .attr('cy', function(d){return d.y})
+    .attr('r', function(d){return d.r}) //size
     .attr('fill', 'red');
+
+}
+Enemies.prototype.update = function(){
+  context = this.createProperties();
+  d3.selectAll('circle')
+    .data(context)
+    .attr('cx', function(d){return d.x}).transition().duration(6500) //positions
+    .attr('cy', function(d){return d.y}).transition().duration(6500)
+    .attr('r', function(d){return d.r}).transition().duration(6500); //size
+}
 
 
 var Player = function(name){
@@ -54,5 +79,24 @@ Player.prototype.createPlayer = function() {
 }
 var player = new Player("Elvio");
 player.createPlayer();
+var enemies = new Enemies();
+var context = enemies.createProperties();
+enemies.createEnemies();
+enemies.update()
+setInterval(function(){enemies.update()}, 7500);
 
 
+
+
+
+
+// .data(context, function(d){
+  //   debugger;
+  //   return d.r;
+  // }).transition().duration(1500)
+  //  .data(context, function(d){
+  //   return d.cx;
+  // }).transition().duration(1500)
+  //   .data(context, function(d){
+  //   return d.cy;
+  // }).transition().duration(1500);
